030bd965e8610004f4e85071fcf3bc7fdc515174
FAteme <miss.l3lue@gmail.com>
2022-07-31 19:33:22 +0430

    Pushing for homework

--- src/Footer.js
+++ src/Footer.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Footer() {
-    return (
-        <div className="row HumidityWind ">
-            <div className="col-lg-8 offset-lg-2 text-center p-0">
-                <hr />
-                <p className="opacity-75">
-                    <a href="https://github.com/gitfateme/react_weather_api">Open-source</a> code by Fateme Sadat Jalilian
-                </p>
-            </div>
-        </div>
-        )
+  return (
+    <div className="row HumidityWind ">
+      <div className="col-lg-8 offset-lg-2 text-center p-0">
+        <hr />
+        <p className="opacity-75">
+          <a href="https://github.com/gitfateme/react_weather_api">
+            Open-source
+          </a>{" "}
+          code by FAteme
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- src/Hero.js
+++ src/Hero.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/Hero.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/Hero.css";
 
 function Hero() {
   return (
-      <div className="row Hero">
-        <div className="col-lg-8 offset-lg-2 border hero-background text-center p-5 ">
-            <p>Saturday, 25 July, 2022, 15:24</p>
-            <h2>30°C</h2>
-            <div className="d-flex  align-items-center justify-content-center">
-                <img src="https://openweathermap.org/img/wn/02d@2x.png"/>
-                <h3>Few Clouds</h3>
-            </div>
-        <h1>London</h1>
+    <div className="row Hero">
+      <div className="col-lg-8 offset-lg-2 border hero-background text-center p-5 ">
+        <p>Saturday, 25 July, 2022, 15:24</p>
+        <h2>30°C</h2>
+        <div className="d-flex  align-items-center justify-content-center">
+          <img src="https://openweathermap.org/img/wn/02d@2x.png" />
+          <h3>Few Clouds</h3>
         </div>
+        <h1 className="pt-3">London</h1>
       </div>
+    </div>
   );
 }
 
